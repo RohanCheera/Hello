53b1575fba59c9412d7fd801d9821c03ed8d3f96
Rohan Cheera <113199793+RohanCheera@users.noreply.github.com>
2024-11-07 19:22:29 +0530

    Update json2html.js
    second

--- json2html.js
+++ json2html.js
@@ -1,29 +1,24 @@
 // json2html.js
+
 export default function json2html(data) {
-  // Get all unique keys from the data array to serve as table headers
-  const headers = [...new Set(data.flatMap(item => Object.keys(item)))];
+  // Start table with data-user attribute
+  let tableHTML = '<table data-user="cheerarohan@gmail.com">';
 
-  // Start building the HTML table
-  let html = `<table data-user="cheerarohan@gmail.com">`;
+  // Table header
+  tableHTML += '<thead><tr><th>Name</th><th>Age</th><th>Gender</th></tr></thead>';
 
-  // Create the header row
-  html += "<thead><tr>";
-  headers.forEach(header => {
-    html += `<th>${header}</th>`;
+  // Table body
+  tableHTML += '<tbody>';
+  
+  data.forEach(item => {
+    tableHTML += '<tr>';
+    tableHTML += `<td>${item.Name}</td>`;
+    tableHTML += `<td>${item.Age}</td>`;
+    tableHTML += `<td>${item.Gender || ''}</td>`; // Handle missing Gender
+    tableHTML += '</tr>';
   });
-  html += "</tr></thead>";
 
-  // Create the table body
-  html += "<tbody>";
-  data.forEach(row => {
-    html += "<tr>";
-    headers.forEach(header => {
-      html += `<td>${row[header] !== undefined ? row[header] : ""}</td>`;
-    });
-    html += "</tr>";
-  });
-  html += "</tbody></table>";
-
-  // Return the HTML table as a string
-  return html;
+  tableHTML += '</tbody></table>';
+  
+  return tableHTML;
 }
